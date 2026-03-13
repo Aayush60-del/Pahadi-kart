b4a8229a02e1e5aa352d548a53ad0873a01b0644
Your Name <negiaayush2024@gmail.com>
2026-03-13 07:35:33 +0530

    bottom to top

--- script.js
+++ script.js
@@ -256,3 +256,16 @@ if (logo) {
         document.querySelector('.boody').scrollIntoView({ behavior: 'smooth' });
     });
 }
+
+document.getElementById('backToTop').addEventListener('click', function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}); 
+
+window.addEventListener('scroll', function () {
+    const btn = document.getElementById('backToTop');
+    if (window.scrollY > 300) {
+        btn.style.display = 'block';
+    } else {
+        btn.style.display = 'none';
+    }
+});
